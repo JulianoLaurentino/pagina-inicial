b70fc4c77ea6d905dcf4a5eff128799ec31b642a
unknown <julianolaurentino@gmail.com>
2021-02-18 13:04:45 -0300

    add files

---
+++ main.js
@@ -0,0 +1,12 @@
+function trocar(elemento){
+    /**document.getElementById("mousemove").innerHTML = "passando o mouse";
+**/
+    elemento.innerhtml = "passando o mouse";
+}
+
+function voltar(elemento){
+    /**document.getElementById("mousemove").innerHTML = "voltando o mouse";
+**/ 
+    elemento.innerhtml = "voltando o mouse";
+
+}
